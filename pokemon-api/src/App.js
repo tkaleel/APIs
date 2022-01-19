44cd6fce5b7c6e2965820f7f1fd99a23531b63f0
theo <91856536+tkaleel@users.noreply.github.com>
2022-01-18 18:20:52 -0800

    add axios

--- pokemon-api/src/App.js
+++ pokemon-api/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
 
   const [pokemon, setPokemon] = useState([]);
 
+  //vanilla js fetch
   const catchPokemon = () => {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=810")
     .then( response => {
@@ -17,18 +19,30 @@ function App() {
     });
   }
 
+  //axios fetch
+  const axiosPokemon = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=810")
+    //axios wraps response in .data object
+    .then(response => {
+      console.log(response.data.results);
+      setPokemon(response.data.results);
+    })
+    .catch(error => console.log(error))
+  }
+
   return (
     <div className="App">
       <h2>Pokemon ╰(*°▽°*)╯</h2>
       <hr />
-      <button onClick={catchPokemon}>Catch Pokemon!</button>
+      <button onClick={catchPokemon}>Gotta catch 'em all!</button>
+      <button onClick={axiosPokemon}>AXIOS Catch 'em all!</button>
       <hr />
       {/* {JSON.stringify(pokemon)} */}
 
       {
         pokemon.map( (mon, index) => {
           return (
-            <p>
+            <p key={index}>
               {mon.name}
             </p>
           ) 
